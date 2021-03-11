0aa66feaff52b21250219f207fbf96d5b7cfbfeb
Marcos Rodrigo <marcos.pereira.ter@estacio.br>
2021-03-11 19:45:58 -0300

    Inclusão do projeto de estudo

---
+++ ProjetoWebExemplo/WebEx/Controllers/AlunoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebEx.Models;
+
+namespace WebEx.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Mensagem = "Funcionou";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Confirmar(AlunoViewModel objView)
+        {
+            if (objView.matricula != "")
+                ViewBag.Mensagem = ViewBag.Mensagem + ": de novo";
+            else
+                ViewBag.Mensagem = ViewBag.Mensagem + ": erro";
+            return View("Index");
+        }
+    }
+}
---
+++ ProjetoWebExemplo/WebEx/Models/AlunoViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebEx.Models
+{
+    public class AlunoViewModel
+    {
+        public string matricula { get; set; }
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string CPF { get; set; }
+    }
+}
